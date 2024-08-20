72d95071867b2eb3259a2202e214f9c1c378c08c
APIVerve Team <hello@apiverve.com>
2024-08-20 11:09:30 -0500

    Bug Fixes

---
+++ SunPositionQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class SunPositionQueryOptions {
+public double lat { get; set; }
+public double lon { get; set; }
+}
+
+}
