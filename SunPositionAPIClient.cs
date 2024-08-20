b0aaa796531bafae702e446fb5d804a2057972d1
APIVerve Team <hello@apiverve.com>
2024-08-20 11:09:28 -0500

    Bug Fixes

--- SunPositionAPIClient.cs
+++ SunPositionAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, sunpositionQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, SunPositionQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(sunpositionQueryOptions options = null)
+        public ResponseObj Execute(SunPositionQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(sunpositionQueryOptions options)
+        private string constructURL(SunPositionQueryOptions options)
         {
             string url = _apiEndpoint;
 
